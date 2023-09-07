066ead97b2badea907c87aa455373268174b53b5
LEONHARD LEUNG <2230618@slu.edu.ph>
2023-09-07 21:03:40 +0800

    Added the methods of the interface

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,2 +1,6 @@
 public interface Sorter {
-}
+    public void sort(String[] array);
+    public void sort(int[] array);
+    public void displayElements(String[] array);
+    public void displayElements(int[] array);
+} // end of Sorter interface
